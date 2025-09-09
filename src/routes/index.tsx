07713d80efeaa815a8f4226651017cc6782c4b62
Cory House <housecor@gmail.com>
2025-09-09 09:28:34 +0200

    a

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,22 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { env } from "../utils/env";
+import ky from "ky";
+import { foodSchema } from "../types/foods.types";
+import Menu from "../Menu";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  );
+  const { data: foods } = useSuspenseQuery({
+    queryKey: ["foods"],
+    queryFn: async () => {
+      const json = await ky.get(env.apiUrl + "/foods").json();
+      return foodSchema.array().parse(json);
+    },
+  });
+
+  return <Menu foods={foods} />;
 }
